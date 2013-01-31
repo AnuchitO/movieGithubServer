a524b1381da6c2fa936a53b0a60bc58616b9cd71
b5302598 <nongmaster_en_com_b5302598@hotmail.com>
2013-01-31 10:54:18 -0800

    added cinema file.

---
+++ cinema/grails-app/composers/cinema/IndexComposer.groovy
@@ -0,0 +1,13 @@
+package cinema
+
+import org.zkoss.zk.grails.composer.*
+
+import org.zkoss.zk.ui.select.annotation.Wire
+import org.zkoss.zk.ui.select.annotation.Listen
+
+class IndexComposer extends GrailsComposer {
+
+    def afterCompose = { window ->
+        // initialize components here
+    }
+}
