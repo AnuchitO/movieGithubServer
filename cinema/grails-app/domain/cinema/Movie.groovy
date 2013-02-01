d45309a797883703a36899effcd164ba03567162
b5315932 <godofcyber666@gmail.com>
2013-02-01 11:50:17 -0800

    resolve conflict

---
+++ cinema/grails-app/domain/cinema/Movie.groovy
@@ -0,0 +1,22 @@
+package cinema
+
+class Movie {
+
+
+	String	movId
+	String 	movType
+	String 	movName
+	String 	movSuperintendent
+	String 	movDetail
+	String 	movCost
+	Date 	movDate
+	Date	movDateEnd
+	String 	movPicture
+
+
+	Rooms	rooms
+
+	
+    static constraints = {
+    }
+}
---
+++ cinema/test/unit/cinema/MovieTests.groovy
@@ -0,0 +1,17 @@
+package cinema
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Movie)
+class MovieTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
