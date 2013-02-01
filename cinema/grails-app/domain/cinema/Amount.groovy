cbef13bd7aba6f7f507b5c3e0574ca145b8526c8
U-CPE57\all user <alluser@cpe22.(none)>
2013-02-01 11:54:08 -0800

    added cinema file

---
+++ cinema/grails-app/domain/cinema/Amount.groovy
@@ -0,0 +1,14 @@
+package cinema
+
+class Amount {
+		Date	date
+		String	firstName
+		String	price
+
+
+		Seats	seats
+		static 	belongsTo  = [individual:Individual]
+
+    static constraints = {
+    }
+}
