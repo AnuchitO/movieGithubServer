ddf233c28e0981a945138817e568329656848182
b5318520 <m_plangklang@hotmail.com>
2013-03-04 00:54:11 +0700

    fixed #4120

--- cinema/grails-app/composers/cinema/IndexShowIndividualComposer.groovy
+++ cinema/grails-app/composers/cinema/IndexShowIndividualComposer.groovy
@@ -8,7 +8,16 @@ import org.zkoss.zk.ui.select.annotation.Listen
 class IndexShowIndividualComposer extends GrailsComposer {
 
     def afterCompose = { window ->
-       
+               		def d = Individual.findAllByVersion(0)
+
+								$('#gid > rows').append{
+									row{ 
+										label(value:d.firstName)
+										label(value:d.lastName) 
+										label(value:d.position)
+										
+									}
+								}
 					
     }
 }
