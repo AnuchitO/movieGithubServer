0453923848bc2b1b6991dc085cdd25e7e7a96034
b5302567 <ke2534@hotmail.com>
2013-03-15 15:14:17 +0700

    fixed #5682

--- cinema/test/unit/cinema/DataDepositTests.groovy
+++ cinema/test/unit/cinema/DataDepositTests.groovy
@@ -11,7 +11,342 @@ import org.junit.*
 @TestFor(DataDeposit)
 class DataDepositTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+    void testSetting() {
+
+    
+    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	     assert a.idDep=="ID-1234"
+		 assert a.numDep=="No.0001"
+		 assert a.timeDep=="13:00:00"
+		 assert a.nameDep=="Masker"
+		 assert a.lnameDep=="Watermark"
+		 assert a.genderDep=="ชาย"
+		 assert a.inDep=="1/1/2556"
+		 assert a.phoneDep=="080-1234567"
+		 assert a.addressDep=="111 ถนนมุกดาอุทิศ"
+		 assert a.emailDep=="aabba@hotmail.com"
+		 assert a.statusDep=="no"
+
+		 assert a.validate()==true
+	}
+
+	void testidDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+	
+	void testnumDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testtimeDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testnameDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testlnameDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testgenderDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testinDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testphoneDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testaddressDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testemailDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void teststatusDepNull() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:""
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testDataNotDuliplicate() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    a.save()
+
+	    def b = new DataDeposit(
+	     idDep:"ID-1234",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    b.save()
+
+	    assert b.validate()==false
+	}
+
+	void testmaxSizeidDep() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-12345",
+		 numDep:"No.0001",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testmaxSizenumDep() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-12345",
+		 numDep:"No.00012",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-1234567",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testmaxSizephoneDep() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-12345",
+		 numDep:"No.00012",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-123456789",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"aabba@hotmail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	void testminSizeemailDep() {
+
+	    def a = new DataDeposit(
+	     idDep:"ID-12345",
+		 numDep:"No.00012",
+		 timeDep:"13:00:00",
+		 nameDep:"Masker",
+		 lnameDep:"Watermark",
+		 genderDep:"ชาย",
+		 inDep:"1/1/2556",
+		 phoneDep:"080-123456789",
+		 addressDep:"111 ถนนมุกดาอุทิศ",
+		 emailDep:"a@mail.com",
+		 statusDep:"no"
+	    )
+	    assert a.validate()==false	
+	}
+
+	
+	    
 }
